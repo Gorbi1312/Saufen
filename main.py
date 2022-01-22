80c56218e656ee56f756442ea4fb571051bea1a9
Dennis Fröse <Dennis.Frose@gmail.com>
2022-01-22 21:27:30 +0100

    added code

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pyautogui as pg
-import timer
+import time
 
+print ("Hello World")
+
+time.sleep(10)
+
+txt = open('Saufen.txt')
+
+a = "Heute wird richtig"
+
+for i in txt:
+    pg.write(a+' '+i)
+    pg.press('Enter')
